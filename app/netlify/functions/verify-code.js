ee2fff0f7e4522212aa5465b75378a76dc1e2712
Lets-go-free <chris@letsgofree.me>
2026-02-21 12:11:13 +0100

    Update verify-code.js

--- app/netlify/functions/verify-code.js
+++ app/netlify/functions/verify-code.js
@@ -14,7 +14,7 @@ const VALID_CODES = [
 const UPDATE_MESSAGE = 'Technisches Update, keine weitere Aktion notwendig.';
 
 // Aktuelle App-Version (muss mit CURRENT_VERSION in index.html übereinstimmen)
-const CURRENT_VERSION = '1.1';
+const CURRENT_VERSION = '1.2';
 
 exports.handler = async function(event) {
   if (event.httpMethod !== 'POST') {
