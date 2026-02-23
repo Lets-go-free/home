4e1807a1c78081393f0b8aa2380cd99c401ea7ce
Lets-go-free <chris@letsgofree.me>
2026-02-23 10:48:26 +0100

    Update sw.js

--- app/sw.js
+++ app/sw.js
@@ -1,5 +1,5 @@
 // ─── Let's go free · Service Worker ───────────────────────────────────────
-const CACHE_VERSION = 'lgf-v68.0';
+const CACHE_VERSION = 'lgf-v69.0';
 const CACHE_NAME = `letsgofree-${CACHE_VERSION}`;
 
 const APP_SHELL = [
