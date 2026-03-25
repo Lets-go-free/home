950dc83ba2763edc320d5304cb8bc85dfa79da3b
Lets-go-free <chris@letsgofree.me>
2026-03-25 22:59:57 +0100

    .

--- app/netlify/functions/verify-code.js
+++ app/netlify/functions/verify-code.js
@@ -10,7 +10,7 @@ const VALID_CODES = [
 //const UPDATE_MESSAGE = 'Technisches Update, keine weitere Aktion notwendig.';
 
 // Aktuelle App-Version (muss mit CURRENT_VERSION in index.html übereinstimmen)
-const CURRENT_VERSION = '1.0.6';
+const CURRENT_VERSION = '1.0.7';
 
 exports.handler = async function(event) {
   if (event.httpMethod !== 'POST') {
